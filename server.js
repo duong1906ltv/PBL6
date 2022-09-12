7269db6dd0d9e43d1b461527486c099c42b0e753
duongpd-0230 <phung.dinh.duong@sun-asterisk.com>
2022-09-12 21:16:53 +0700

    [Set up] Connect DB

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require("express");
+import express from "express"
 const app = express();
-const PORT = 4000;
 
-app.get("/api", (req, res) => {
-  res.json({
-    message: "Hello world",
-  });
-});
+import dotenv from "dotenv";
+dotenv.config();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+import connectDB from "./db/connect.js";
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
